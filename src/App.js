9fdaa9c1e7a22ae8bb5cd873c19daefd3e8fd2c5
Hein Ko Zin <heinkozin4@gmail.com>
2020-04-25 01:57:52 +0630

    final project

--- src/App.js
+++ src/App.js
@@ -15,51 +15,49 @@ const render = new marked.Renderer();
 class App extends React.Component {
   state = {
     text: `# Welcome to my React Markdown Previewer!
-  
-    ## This is a sub-heading...
-    ### And here's some other cool stuff:
+## H2...
+### H3:
       
-    Heres some code, \`<div></div>\`, between 2 backticks.
+\`<div></div>\`, between 2 backticks.
     
-    \`\`\`
-    // this is multi-line code:
-    
-    function anotherExample(firstLine, lastLine) {
-      if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-        return multiLineCode;
-      }
-    }
-    \`\`\`
+\`\`\`
+// this is multi-line code:
+
+function greeting(userName){
+  return "Hello, "+userName;
+}
+
+\`\`\`
       
-    You can also make text **bold**... whoa!
-    Or _italic_.
-    Or... wait for it... **_both!_**
-    And feel free to go crazy ~~crossing stuff out~~.
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
     
-    There's also [links](https://www.freecodecamp.com), and
-    > Block Quotes!
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
     
-    And if you want to get really crazy, even tables:
+And if you want to get really crazy, even tables:
     
-    Wild Header | Crazy Header | Another Header?
-    ------------ | ------------- | ------------- 
-    Your content can | be here, and it | can be here....
-    And here. | Okay. | I think we get it.
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
     
-    - And of course there are lists.
-      - Some are bulleted.
-         - With different indentation levels.
-            - That look like this.
+- And of course there are lists.
+- Some are bulleted.
+- With different indentation levels.
+- That look like this.
     
     
-    1. And there are numbererd lists too.
-    1. Use just 1s if you want! 
-    1. But the list goes on...
-    - Even if you use dashes or asterisks.
-    * And last but not least, let's not forget embedded images:
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
     
-    ![React Logo w/ Text](https://goo.gl/Umyytc)
-    `
+![React Logo w/ Text](https://cdn.dribbble.com/users/14374/screenshots/3109703/angular_brand_angular.png)
+`
   };
 
   textarea = React.createRef();
